48b9474ddae0aa91c1e49a91848dc1204380d048
Arsen Makovei <makoveyarsen@gmail.com>
2025-12-13 01:11:15 +0200

    Fix docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_BACKEND_URL || 'http://backend:8174',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
